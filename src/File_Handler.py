5fe6ca0cb7fa40e63a46fcf08418d51bc3d58f53
tonytonfisk2 <tony6781@hotmail.com>
2025-12-21 16:19:51 +0100

    update

--- src/File_Handler.py
+++ src/File_Handler.py
@@ -1,14 +1,54 @@
 import os
 import glob
+import shutil
+import yaml
+from Extract import extract
+from Preprocess import preprocess
+from Search import search
+
 
 def extract_files(dir_path):
+    files = glob.glob(os.path.join(dir_path, '*.pdf'))
+
+    return files
+
+def rename_files(dir_path, pdf_type):
+
+    with open('../config/keywords.yaml', 'r', encoding='UTF-8') as file:
+        keywords = yaml.safe_load(file)
+
+    invoice_field = keywords[pdf_type]
+
+    v_map = keywords.get('vat_mapping', {})
+
     processed_dir = os.path.join(dir_path, 'processed')
     if not os.path.exists(processed_dir):
         os.makedirs(processed_dir)
 
-    files = glob.glob(os.path.join(dir_path, '*.pdf'))
+    try:
+        files = extract_files(dir_path)
+        for file_path in files:
+            result = {}
 
-    return files, processed_dir
+            text = extract(file_path)
+            text = preprocess(text)
+            
+            for k, k_list in invoice_field.items():
+                s = search(text, k_list, k, v_map)
+                result.update(s)
+            
+            required_fields = [key for key, _ in invoice_field.items()]
+            missing_fields = [field for field in required_fields if field not in result or not result[field]]
+            
+            if missing_fields:
+                print(f'Missing fields {missing_fields} for {os.path.basename(file_path)}')
+                continue
 
-def rename_files(dir_path):
-    return None
+            new_filename = f"{result['Datum']} {result['Företagnamn'].title()} {result['Fakturanummer'].title()}"
+            new_filepath = os.path.join(processed_dir, new_filename)
+
+            shutil.copy2(file_path, new_filepath)
+
+            print(result)
+    except Exception as e:
+        print(str(e))
--- src/Main.py
+++ src/Main.py
@@ -2,10 +2,7 @@ import os
 import shutil
 import yaml
 
-from Extract import extract
-from Preprocess import preprocess
-from Search import search
-from File_Handler import extract_files
+from File_Handler import rename_files
 from integrations.mail_handler import download_pdfs 
 
 # Download attachments 
@@ -16,44 +13,13 @@ def main():
 
     DIR_PATH = '../data'
     PDF_TYPE = 'invoice'
+    DOWNLOAD = False
 
-    with open('../config/keywords.yaml', 'r', encoding='UTF-8') as file:
-        keywords = yaml.safe_load(file)
+    if DOWNLOAD:
+        download_pdfs()
 
-    invoice_field = keywords[PDF_TYPE]
+    rename_files(dir_path= DIR_PATH, pdf_type= PDF_TYPE)
 
-    vat_mapping = keywords.get('vat_mapping', {})
-
-    #print(download_pdfs())
-
-    files, pf = extract_files(DIR_PATH)
-
-    for file_path in files:
-        try:
-            result = {}
-
-            text = extract(file_path)
-            text = preprocess(text)
-            for k, k_list in invoice_field.items():
-                s = search(text, k_list, k, vat_mapping)
-                result.update(s)
-
-            required_fields = ['Datum', 'Företagnamn', 'Fakturanummer']
-            missing_fields = [field for field in required_fields if field not in result or not result[field]]
-
-            if missing_fields:
-                print(f"   WARNING: Missing fields {missing_fields} for {os.path.basename(file_path)}")
-                print(f"   Skipping file rename.\n")
-                continue
-           
-            new_filename = f"{result['Datum']} {result['Företagnamn'].title()} {result['Fakturanummer'].title()}.pdf"
-            new_filepath = os.path.join(pf, new_filename)
-
-            shutil.copy2(file_path, new_filepath)
-
-            print(result)
-        except Exception as e:
-            print(str(e))
 
 if __name__ == "__main__":
     main()
